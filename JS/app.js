7be22f260e9325d759484fd430d25e1c3ccc79fe
vbhv4991GitHub <ravi9406126068@gmail.com>
2021-02-14 19:48:14 +0530

    Writing algorithm to ring the alarm in JavaScript

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,36 @@
 console.log("Hello World.");
+
+let alarmTime = document.getElementById('alarmTime');
+
+const ringAlarm = function(url) {
+    let audio = new Audio(url);
+    audio.play();
+}
+
+const setAlarm = function(e) {
+    e.preventDefault(); // Stops the page from reloading upon submitting the form.
+    console.log("Setting up your alarm.");
+    let date = document.getElementById('date');
+    let aTime = document.getElementById('aTime');
+    console.log(date.value);
+    console.log(aTime.value);
+    let timeofTheAlarm = new Date(`${date.value} ${aTime.value}`);
+    console.log(timeofTheAlarm);
+
+    let now = new Date();
+    let timeToAlarm = timeofTheAlarm - now;
+
+    console.log(timeToAlarm);
+
+    if(timeToAlarm>=0) {
+        setTimeout(() =>{
+            ringAlarm(url);
+        }, timeToAlarm);
+    }
+    else {
+        console.log("Please enter a valid date and time.");
+    }
+
+}
+
+alarmTime.addEventListener('click', setAlarm);
