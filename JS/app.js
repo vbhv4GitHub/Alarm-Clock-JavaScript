57671b7d861d531269dff2fa26468aba53f8a4a9
vbhv4991GitHub <ravi9406126068@gmail.com>
2021-02-14 23:22:39 +0530

    Need to add a valid audio file URL and it is done.

--- JS/app.js
+++ JS/app.js
@@ -1,34 +1,39 @@
-console.log("Hello World.");
+// * console.log("Hello World.");
 
 let alarmTime = document.getElementById('alarmTime');
 
-const ringAlarm = function(url) {
+// => Creating the function to ring the alarm tone: It accepts a parameter which could be url to an audio file or location of an audio file in your local directory.
+const ringAlarm = function (url) {
     let audio = new Audio(url);
     audio.play();
 }
 
-const setAlarm = function(e) {
+const setAlarm = function (e) {
     e.preventDefault(); // Stops the page from reloading upon submitting the form.
-    console.log("Setting up your alarm.");
     let date = document.getElementById('date');
     let aTime = document.getElementById('aTime');
-    console.log(date.value);
-    console.log(aTime.value);
     let timeofTheAlarm = new Date(`${date.value} ${aTime.value}`);
-    console.log(timeofTheAlarm);
-
     let now = new Date();
+
     let timeToAlarm = timeofTheAlarm - now;
-
-    console.log(timeToAlarm);
-
-    if(timeToAlarm>=0) {
-        setTimeout(() =>{
+    if (timeToAlarm >= 0) {
+        setTimeout(() => {
+            // --> The below function call requires a valid audio url or a valid audio file location path.
             ringAlarm(url);
         }, timeToAlarm);
+        let alertHTML = `<div class="alert alert-success alert-dismissible fade show" role="alert">
+                            <strong>Alarm set!!!</strong> Alarm is successfully set for ${timeofTheAlarm}.
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>`;
+        document.getElementById('alert').innerHTML = alertHTML;
     }
     else {
         console.log("Please enter a valid date and time.");
+        let alertHTML = `<div class="alert alert-danger alert-dismissible fade show" role="alert">
+                            <strong>Error setting up the alarm!!!</strong> Please enter valid date and time.
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>`;
+        document.getElementById('alert').innerHTML = alertHTML;
     }
 
 }
